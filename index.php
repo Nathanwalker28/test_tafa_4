f60b40d853cbc7d9366b6c3ec8ab3e6ce496cca6
nathanwalker28 <falimanananalim@gmail.com>
2022-09-16 08:19:04 +0300

    test base php

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+    $legumes = ["anana", "ovy", 8, 9, "karoty", 4];
+    foreach ($legumes as $value) {
+        if (is_string($value)) {
+            unset($legumes, $value);  
+        }
+    }
+
+    
+
+    
+
+
+    /****************************************/
+
+    // $devises = [
+    //     "eur" => [15, 32, 25, 22, 5],
+    //     "dollars" => [36, 42, 28, 35, 8]
+    // ];
+    
+    // array_push($devises)
+    // foreach($devises["eur"] as $value) {
+    //     array_push($devises["ariary_eur"], $value * 4500);
+    // }
+
+    
+    // var_dump($devises);
+
+    /******************************* */
+    // $persons = [
+    //     ["Rakoto", "Rasoa"],
+    //     ["Rabe", "Koto"]
+    // ];
+    
+
+    // $persons[1] = "toi";
+
+    // var_dump($persons);
+
+    
+
+    
+
+
+
+    
+
